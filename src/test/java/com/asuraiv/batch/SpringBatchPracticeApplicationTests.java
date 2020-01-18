a0a39df1efd2eb94a723ab6ec63fc5a7a824f95b
asuraiv <asuraiv7@gmail.com>
2020-01-18 22:02:08 +0900

    Initial commit

---
+++ src/test/java/com/asuraiv/batch/SpringBatchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asuraiv.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
